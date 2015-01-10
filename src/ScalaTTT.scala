9accb9dbe10a19449a3ffc7b33a88b334bfff55c
Dan Scarafoni <dan@scarafoni.com>
2015-01-10 00:09:36 -0500

    now successful test and project layout

---
+++ src/ScalaTTT.scala
@@ -0,0 +1,11 @@
+/**
+ * the driver class
+ */
+import game.IO
+object ScalaTTT {
+
+  def main(args: Array[String]) {
+    val io = new game.IO
+    io.test()
+  }
+}
---
+++ src/game/IO.scala
@@ -0,0 +1,11 @@
+package game
+
+/**
+ * has the methods for interacting with the game and such
+ */
+class IO {
+
+  def test() {
+    println("this is a test")
+  }
+}
